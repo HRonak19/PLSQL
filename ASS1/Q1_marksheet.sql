aaba83652c5893ed665777b72d06fb8427577457
[HRonak19] <[hedambharonak@gmail.com]>
2026-01-01 14:58:05 +0530

    new file add

--- ASS1/Q1_marksheet.sql
+++ ASS1/Q1_marksheet.sql
@@ -5,7 +5,35 @@ subjects, total of 5 subjects and percentage, also display the result and class 
 
 DECLARE 
 
+    seat number := &seat_Number;
+    name varchar2(15) := '&name';
+
+        sub1 number := &s1;
+        sub2 number := &s2;
+        sub3 number := &s3;
+        sub4 number := &s4;
+        sub5 number := &s5;
+
+    total number;
+    per number(5,2);
+
 BEGIN
+
+    dbms_output.put_line('SEAT NUMBER : '||seat);
+    dbms_output.put_line('name : '||name);
+        
+        dbms_output.put_line('sub 1 : '||sub1);
+        dbms_output.put_line('sub 2 : '||sub2);
+        dbms_output.put_line('sub 3 : '||sub3);
+        dbms_output.put_line('sub 4 : '||sub4);
+        dbms_output.put_line('sub 5 : '||sub5);
+
+    total := (sub1 + sub2 + sub3 + sub4 + sub5);
+  
+    per := Trunc(total/5);
+
+    dbms_output.put_line('total : '||total);
+    dbms_output.put_line('per : '||per||'%');
 
 END;
 /
